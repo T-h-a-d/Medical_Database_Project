850f6452198b96b4fd12123eeb11450fbf43fd8c
Thad Sauter <thadsauter@gmail.com>
2018-03-13 12:59:55 -0700

    Updated create-table route

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var express	= require('express');
 var mysql = require('mysql');
 var bodyParser = require('body-parser');
 var app = express();
+var create = require('./create_table.js')
 var path = require('path');
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 var root = {root : path.join(__dirname, '/Public')};
 var pool = mysql.createPool({
 	connectionLimit : 10,
@@ -13,17 +15,39 @@ var pool = mysql.createPool({
 });
 
 app.use(express.static(path.join(__dirname, '/Public')))
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.set('mysql', mysql);
-app.set('port', 3000);
+app.set('port', process.argv[2]);
 
-app.get('/', function(){
 
-	
 
+app.get('/create-table', function(res, req, next){
+		create.create_table();
 });
 
-app.listen('3000', function(){
+
+app.get('/', function(res, req, next){
+	res.render('index');
+})
+
+
+
+
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
 	console.log('Server is up and running ... ');
 })
